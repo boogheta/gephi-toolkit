825c956d521b212a440a84edcf80c60d15c26809
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2014-02-20 19:04:56 +0100

    oups, should stay in the good branch

--- build_toolkit.sh
+++ build_toolkit.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# This should already have been done before or the current file wouldn't even be there
+git checkout builder-0.8.2
 # Pull the version we want to try with the toolkit, checkout the desired branch and compile it
 git clone git@github.com:jacomyma/gephi
 cd gephi
@@ -8,7 +10,6 @@ mvn clean install || exit 1
 cd ..
 
 # Adapt the toolkit repo to the corresponding version branch
-git checkout 0.8.2
 # Set the distant repository to be the local maven installation directory
 sed "s/##USER##/$(whoami)/" pom_homebuild.xml > pom.xml
 mvn clean install && echo "Toolkit successfully built and available in $pwd/target/"
